5f936f708985f8ac049959feb62e2d88561d0cc9
Gregory Lureau <g.lureau@betclicgroup.com>
2020-04-20 13:08:27 +0200

    Use Guava directly.

--- app/src/test/java/com/glureau/agp_3_6_javafileobjects_forresources/ExampleUnitTest.kt
+++ app/src/test/java/com/glureau/agp_3_6_javafileobjects_forresources/ExampleUnitTest.kt
@@ -1,11 +1,13 @@
 package com.glureau.agp_3_6_javafileobjects_forresources
 
+import com.google.common.io.Resources
 import com.google.testing.compile.JavaFileObjects
 import org.junit.Test
 
 class ExampleUnitTest {
     @Test
     fun JavaFileObjects_forResources_is_working() {
+        println(Resources.getResource(ExampleUnitTest::class.java, "FindMe.java"))
         println(JavaFileObjects.forResource("FindMe.java"))
     }
 }
